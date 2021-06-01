7a8ce5ae68b4d653a9a8536b7a99e0defbf57115
swajaydeep@gmail.com <6575@jay>
2021-06-01 13:55:59 +0530

    debugged

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
-var Engine = Matter.Engine,
+const  Engine = Matter.Engine,
   World = Matter.World,
   Events = Matter.Events,
   Bodies = Matter.Bodies;
- 
+
+var engine , world;
 var particles = [];
 var plinkos = [];
-
+var divisions = [];
 var divisionHeight=300;
 var score =0;
 function setup() {
@@ -54,7 +55,7 @@ function setup() {
 function draw() {
   background("black");
   textSize(20)
- //text("Score : "+score,20,30);
+  text("Score : "+score,20,30);
   Engine.update(engine);
  
   
@@ -64,7 +65,7 @@ function draw() {
      
    }
    if(frameCount%60===0){
-     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
+     particles.push (new Particle(random(width/2-30, width/2+30), 10,10));
      score++;
    }
  
